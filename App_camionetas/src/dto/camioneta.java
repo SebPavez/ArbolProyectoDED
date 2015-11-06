08fb302c9cabe0205d5b97c06cbda921337a626c
nicolas182 <nicolas.pineda1992@gmail.com>
2015-11-05 22:38:49 -0300

    claseCamcioneta

---
+++ App_camionetas/src/dto/camioneta.java
@@ -0,0 +1,13 @@
+
+package dto;
+
+public class camioneta {
+    
+    private String patente;
+    private String marca;
+    private int añoFabricacion;
+    private String color;
+    private double tonelaje;
+    private String tipoCombustible; 
+    
+}
