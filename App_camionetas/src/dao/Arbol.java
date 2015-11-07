aeb92b55a57ce222b5c6600d505ee28ab0620426
nicolas182 <nicolas.pineda1992@gmail.com>
2015-11-07 18:58:56 -0300

    Arbol
    se agrego algo a la Clase arbol

---
+++ App_camionetas/src/dao/Arbol.java
@@ -0,0 +1,38 @@
+
+package dao;
+import dto.Camioneta;
+
+public class Arbol <T>{
+    
+    private Nodo raiz;
+    
+    public Arbol(){
+        this.raiz = null;
+    }
+    
+    public void agregarNodo(Camioneta auto){
+        if(this.raiz == null){
+            this.raiz = new Nodo(auto);
+        }else{
+            this.raiz.insertar(auto);
+        }
+    }
+    
+    public void preOrden(Nodo nodo){
+        if(nodo == null){
+            return;
+        }
+        System.out.println(nodo.getDato()+" ");
+        this.preOrden(nodo.getNodoIzq());
+        this.preOrden(nodo.getNodoDer());
+    }
+    
+    public void recorrerPreOrden() {
+        this.preOrden(this.raiz);
+    }
+  
+    public Nodo getRaiz(){
+        return raiz;
+    }
+    
+}
---
+++ App_camionetas/src/dao/CamionetaDao.java
@@ -0,0 +1,23 @@
+
+package dao;
+import dto.Camioneta;
+
+public class CamionetaDao {
+private Arbol camionetas;
+
+private Arbol<Nodo> arbol;
+
+public CamionetaDao(){
+    this.camionetas = new Arbol();
+}
+
+public void agregarCamineta(Camioneta camioneta){
+    this.camionetas.agregarNodo(camioneta);
+}
+
+public void mostrarCamionetas(){
+    this.camionetas.recorrerPreOrden();
+}
+    
+    
+}
